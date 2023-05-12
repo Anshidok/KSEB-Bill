7f63632b95baa99a2fb4807d949991924abb7bee
Muhammed Anshid OK <anshid283@gmail.com>
2023-05-12 15:20:17 +0530

    kseb bill

---
+++ src/electricitybill/Electricitybill.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package electricitybill;
+
+/**
+ *
+ * @author anshid
+ */
+public class Electricitybill {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //addusers m =new addusers();
+        //.setVisible(true);
+          //addusers m =new addusers();
+        //m.setVisible(true);
+         //profile m =new profile();
+        //m.setVisible(true);
+        adminhome m =new adminhome();
+        m.setVisible(true);
+        
+    }
+    
+}
